d9ee97e8f1ac583dcb52317bff5abde227f90351
Lucas Lentz <lucas.lentz@hotmail.com>
2018-03-22 13:04:29 -0300

    refactor some stuff

--- app/app.js
+++ app/app.js
@@ -1,38 +1,47 @@
-const canvas = document.getElementById('drawer'),
-  ctx = canvas.getContext('2d'),
-  reader = new FileReader(),
-  drakeIMG = new Image(), 
-  albumIMG = new Image(),
-  btnGenerate = document.getElementById('btnGenerate'),
-  btnAlbum = document.getElementById('btnAlbum'),
-  ALBUM_POSITION_GLOBAL_X = 620;
-  ALBUM_POSITION_GLOBAL_Y = -120;
-  ALBUM_POSITION_X = 300, 
-  ALBUM_POSITION_Y = 260,
-  ALBUM_SIZINGS = 180,
-  ALBUM_ANGLE_DEGREES = 75;
+(() => {
+  const canvas = document.getElementById('drawer'),
+    ctx = canvas.getContext('2d'),
+    ALBUM_POSITION_GLOBAL_X = 620;
+    ALBUM_POSITION_GLOBAL_Y = -120;
+    ALBUM_POSITION_X = 300, 
+    ALBUM_POSITION_Y = 260,
+    ALBUM_SIZINGS = 180,
+    ALBUM_ANGLE_DEGREES = 75;
+    
+  const drakeIMG = new Image();
 
-const start = () =>  {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.save();
-  ctx.drawImage(drakeIMG, 0, 0);
-};
+  drakeIMG.src = "app/assets/main-image.jpg";
+  drakeIMG.addEventListener('load', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(drakeIMG, 0, 0);
+    ctx.save();
+  });
 
-drakeIMG.src = "app/assets/main-image.jpg";
-drakeIMG.onload = start;
-  
-const rotateAndPlaceImage = (image, angle) => {
-  reader.onload = event => {
-    albumIMG.src = reader.result;
+  const btnAlbum = document.getElementById('btnAlbum');
+
+  btnAlbum.addEventListener('change', e => {
+    if(e.target.files.length) {
+      readFile(e.target.files[0])
+        .then(drawAlbum);
+    }
+  });
+
+  const readFile = file => new Promise(resolve => {
+    const reader = new FileReader();
+    
+    reader.addEventListener('load', () => {
+      resolve(reader.result);
+    });
+
+    reader.readAsDataURL(file);
+  });
+
+  const drawAlbum = img => {
+    const albumIMG = new Image();
+    albumIMG.src = img;
     ctx.translate(ALBUM_POSITION_GLOBAL_X, ALBUM_POSITION_GLOBAL_Y);
-    ctx.rotate(angle * Math.PI / 180);
-    ctx.drawImage(image, ALBUM_POSITION_X, ALBUM_POSITION_Y, ALBUM_SIZINGS, ALBUM_SIZINGS);
+    ctx.rotate(ALBUM_ANGLE_DEGREES * Math.PI / 180);
+    ctx.drawImage(albumIMG, ALBUM_POSITION_X, ALBUM_POSITION_Y, ALBUM_SIZINGS, ALBUM_SIZINGS);
     ctx.restore();
   };
-};
-
-btnGenerate.addEventListener('click', () => {
-  reader.readAsDataURL(btnAlbum.files[0]);    
-  start();
-  rotateAndPlaceImage(albumIMG, ALBUM_ANGLE_DEGREES);
-});
+})();
